733b66c1dea01faf8542e286e57cbc77b6c6138a
Filip Dratwiński <filip.dratwinski@interia.pl>
2020-10-20 18:53:27 +0200

    Add requirements, add example of connecting to rest api of taggers

--- taggers.py
+++ taggers.py
@@ -1,17 +1,27 @@
 import requests
+import json
+import xml.etree.ElementTree as ET
 
 url = "http://ws.clarin-pl.eu/nlprest2/base/process"
-payload = "{\n    \"text\": \"Ala ma kota i wszyscy jej zazdroszczą. Jej kot jest leniwy, ale nikt o tym nie wie.\",\n    \"lpmn\": \"morphoDita\",\n    \"user\": \"xxx@yyy.com\"\n}"
+payload = json.dumps({"text": open("data\\test\\Albania_138293.txt", "r",encoding="utf-8").read(),
+           "lpmn": "morphoDita",
+           "user": "xxx@yyy.com"})
 headers = {
   'Content-Type': 'application/json'
 }
 
 response = requests.request("POST", url, headers=headers, data = payload)
 
-print(response.text.encode('utf8'))
+xml = response.text.encode('utf8')
+myfile = open("morphoDita.xml", "wb")
+myfile.write(xml)
 
-payload = "{\n    \"text\": \"Ala ma kota i wszyscy jej zazdroszczą. Jej kot jest leniwy, ale nikt o tym nie wie.\",\n    \"lpmn\": \"wcrft2\",\n    \"user\": \"xxx@yyy.com\"\n}"
 
+payload = json.dumps({"text": open("data\\test\\Albania_138293.txt", "r",encoding="utf-8").read(),
+           "lpmn": "wcrft2",
+           "user": "xxx@yyy.com"})
 response = requests.request("POST", url, headers=headers, data = payload)
 
-print(response.text.encode('utf8'))
+xml = response.text.encode('utf8')
+myfile = open("wcrft2.xml", "wb")
+myfile.write(xml)
