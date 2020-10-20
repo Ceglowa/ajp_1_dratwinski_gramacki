1b1b20ae81783a8a17da96da764653f5fb865933
Filip Dratwiński <filip.dratwinski@interia.pl>
2020-10-20 17:30:21 +0200

    Fix segmentator to save result to json file

--- segmentator.py
+++ segmentator.py
@@ -1,10 +1,14 @@
 import re
+import json
 
-def segmentate(text):
+def segmentate(file_name):
+
+
+    text = open(file_name, "r",encoding="utf-8").read()
+
     sentences = []
-
     begin_of_sentence = 0
-    end_of_sentence = None
+
 
     for index in range(len(text)):
         if begin_of_sentence is None and text[index].isupper():
@@ -16,19 +20,14 @@ def segmentate(text):
             is_shortcut = re.search("it[pd]|inż|inz|ur", last_word_in_sentence)
 
             if is_shortcut is None:
-                end_of_sentence = index
-                sentences.append(text[begin_of_sentence:end_of_sentence+1])
+                sentences.append({"text":text[begin_of_sentence:index+1]})
 
                 begin_of_sentence=None
 
-
-    print(sentences)
-
     begin_of_word = 0
-    end_of_word = None
-    words = []
-
-    for sentence in sentences:
+    for sen_index in range(len(sentences)):
+        sentence = sentences[sen_index]['text']
+        words_for_sentence = []
         for index in range(len(sentence)):
             if begin_of_word is None:
                 if re.search("[0-9a-zA-ZąćęńłóśżźĄĆĘŃŁÓŚŻŹ]", sentence[index]):
@@ -38,16 +37,14 @@ def segmentate(text):
 
                 if not re.search("[0-9a-zA-ZąćęńłóśżźĄĆĘŃŁÓŚŻŹ][^ ][0-9a-zA-ZąćęńłóśżźĄĆĘŃŁÓŚŻŹ]", sentence[index-1:index+2]):
                     word = sentence[begin_of_word:index]
-
-                    end_of_word = index
-                    words.append(word)
-
+                    words_for_sentence.append(word)
                     begin_of_word=None
+        sentences[sen_index]['words'] = words_for_sentence
 
-    print(words)
+    with open('segmented_text.json', 'w') as fout:
+        json.dump(sentences, fout)
 
 if __name__ == '__main__':
     file_name = "data/train/Katolicyzm_2850522.txt"
-    f = open(file_name, "r",encoding="utf-8")
-    segmentate(f.read())
+    segmentate(file_name)
 
