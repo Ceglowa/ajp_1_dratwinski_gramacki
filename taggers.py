785ca12dacdd13051eba9e25e9d601dd0348544e
Piotr Gramacki <p.gramacki@gmail.com>
2020-10-20 18:11:49 +0200

    add morphodita and wcrft2

---
+++ taggers.py
@@ -0,0 +1,17 @@
+import requests
+
+
+payload = "{\n    \"text\": \"Ala ma kota i wszyscy jej zazdroszczą. Jej kot jest leniwy, ale nikt o tym nie wie.\",\n    \"lpmn\": \"morphoDita\",\n    \"user\": \"xxx@yyy.com\"\n}"
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+payload = "{\n    \"text\": \"Ala ma kota i wszyscy jej zazdroszczą. Jej kot jest leniwy, ale nikt o tym nie wie.\",\n    \"lpmn\": \"wcrft2\",\n    \"user\": \"xxx@yyy.com\"\n}"
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
