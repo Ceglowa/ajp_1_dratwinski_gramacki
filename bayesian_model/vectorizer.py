28a4c253fd2f18999ab0378f4efd065d956d74ec
Filip Dratwiński <filip.dratwinski@interia.pl>
2020-11-02 00:18:06 +0100

    Add rules for evey case

--- bayesian_model/vectorizer.py
+++ bayesian_model/vectorizer.py
@@ -40,6 +40,14 @@ def extract_words_based_on_rules(words_list: List[List[Tuple[str, str]]], rules_
 
 def get_vectorized_data(tagger_name: str, rules: List):
     train_words, train_labels, test_words, test_labels = read_pickled_data(PATH_TO_PICKLES + os.path.sep + f"{tagger_name}.pkl")
+    train_lic_idx = train_labels.index("license.xml")
+    test_lic_idx = test_labels.index("license.xml")
+
+    train_words.pop(train_lic_idx)
+    train_labels.pop(train_lic_idx)
+
+    test_words.pop(test_lic_idx)
+    test_labels.pop(test_lic_idx)
 
     train_words_filtered_POS = extract_words_based_on_rules(train_words, rules)
     test_words_filtered_POS = extract_words_based_on_rules(test_words, rules)
@@ -56,12 +64,35 @@ def get_vectorized_data(tagger_name: str, rules: List):
     y_train = encoder.fit_transform(train_labels)
     y_test = encoder.transform(test_labels)
 
-    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
+    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test), encoder.classes_
 
 
 if __name__ == '__main__':
+    nouns_rules = [lambda tag: tag.startswith("subst"),
+                   lambda tag: tag.startswith("depr"),
+                   lambda tag: tag.startswith("ger")]
+
+    verb_rules = [lambda tag: tag.startswith("fin"),
+                  lambda tag: tag.startswith("bedzie"),
+                  lambda tag: tag.startswith("aglt"),
+                  lambda tag: tag.startswith("praet"),
+                  lambda tag: tag.startswith("impt"),
+                  lambda tag: tag.startswith("imps"),
+                  lambda tag: tag.startswith("inf"),
+                  lambda tag: tag.startswith("pcon"),
+                  lambda tag: tag.startswith("pan")]
+
+    adj_rules = [lambda tag: tag.startswith("adj"),
+                 lambda tag: tag.startswith("adja"),
+                 lambda tag: tag.startswith("adjp"),
+                 lambda tag: tag.startswith("adjc"),
+                 lambda tag: tag.startswith("pact"),
+                 lambda tag: tag.startswith("ppas"),]
+
+
+
     for tagger in ['krnnt', 'morpho', 'wcrft2']:
-        X_train, y_train, X_test, y_test = get_vectorized_data(tagger, [lambda tag: tag.startswith("subst")])
+        X_train, y_train, X_test, y_test, labels = get_vectorized_data(tagger, nouns_rules)
 
         print("got data")
 
@@ -70,8 +101,5 @@ if __name__ == '__main__':
 
         y_pred = clf.predict(X_test)
 
-        print(tagger)
-        print(classification_report(y_true=y_test, y_pred=y_pred))
-
-
-
+        with open(f"nouns_{tagger}.txt", 'w') as f:
+            print(classification_report(y_true=y_test, y_pred=y_pred, labels=labels), file=f)
