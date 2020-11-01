e4191758dd15ad239eb980cf79a4b1b567953baf
Filip Dratwiński <filip.dratwinski@interia.pl>
2020-11-01 13:08:02 +0100

    Generalize two methods

--- bayesian_model/processor.py
+++ bayesian_model/processor.py
@@ -11,68 +11,43 @@ KRNNT_FOLDER = "krnnt"
 WCRFT2_FOLDER = "wcrft2"
 
 
-def process_training_data_to_xmls(path_to_data: str):
-    training_data_files = os.listdir(path_to_data + os.path.sep + 'train')
-    path_to_processed_train_files = PATH_TO_RAW_FILES + os.path.sep + "processed_train"
+def process_data_to_xmls(path_to_data: str, dataset_part: str):
+    data_files = os.listdir(path_to_data + os.path.sep + dataset_part)
+    path_to_processed_files = PATH_TO_RAW_FILES + os.path.sep + f"processed_{dataset_part}"
 
-    for training_data_file in training_data_files:
-        if training_data_file.endswith(".txt") and not os.path.isfile(
-                path_to_processed_train_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
-                training_data_file.split(".")[:-1][0] + ".xml"):
-            text = open(PATH_TO_RAW_FILES + os.path.sep + "train" + os.path.sep + training_data_file, "r",
+    for data_file in data_files:
+        if data_file.endswith(".txt") and not os.path.isfile(
+                path_to_processed_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
+                data_file.split(".")[:-1][0] + ".xml"):
+            text = open(PATH_TO_RAW_FILES + os.path.sep + dataset_part + os.path.sep + data_file, "r",
                         encoding="utf-8").read()
-            get_tagging_from_krnnt(text, path_to_processed_train_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                   training_data_file.split(".")[:-1][0] + ".xml")
+            get_tagging_from_krnnt(text, path_to_processed_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
+                                   data_file.split(".")[:-1][0] + ".xml")
 
-            convert_krnnt_output_file_to_xml(path_to_processed_train_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                             training_data_file.split(".")[:-1][0] + ".xml",
-                                             path_to_processed_train_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                             training_data_file.split(".")[:-1][0] + ".xml"
+            convert_krnnt_output_file_to_xml(path_to_processed_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
+                                             data_file.split(".")[:-1][0] + ".xml",
+                                             path_to_processed_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
+                                             data_file.split(".")[:-1][0] + ".xml"
                                              )
 
             get_tagging_from_morphoDita(text,
-                                        path_to_processed_train_files + os.path.sep + MORPHODITA_FOLDER + os.path.sep +
-                                        training_data_file.split(".")[:-1][0] + ".xml")
-            get_tagging_from_wcrft2(text, path_to_processed_train_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
-                                    training_data_file.split(".")[:-1][0] + ".xml")
+                                        path_to_processed_files + os.path.sep + MORPHODITA_FOLDER + os.path.sep +
+                                        data_file.split(".")[:-1][0] + ".xml")
+            get_tagging_from_wcrft2(text, path_to_processed_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
+                                    data_file.split(".")[:-1][0] + ".xml")
 
 
-def process_testing_data_to_xmls(path_to_data: str):
-    test_data_files = os.listdir(path_to_data + os.path.sep + 'test')
-    path_to_processed_test_files = PATH_TO_RAW_FILES + os.path.sep + "processed_test"
 
-    for test_data_file in test_data_files:
-        if test_data_file.endswith(".txt") and not os.path.isfile(
-                path_to_processed_test_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
-                test_data_file.split(".")[:-1][0] + ".xml"):
-            text = open(PATH_TO_RAW_FILES + os.path.sep + "test" + os.path.sep + test_data_file, "r",
-                        encoding="utf-8").read()
-            get_tagging_from_krnnt(text, path_to_processed_test_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                   test_data_file.split(".")[:-1][0] + ".xml")
+def read_data(path_to_data: str, tagger_folder: str, dataset_part: str):
+    data_files = os.listdir(path_to_data + os.path.sep + f'processed_{dataset_part}' + os.path.sep + tagger_folder)
 
-            convert_krnnt_output_file_to_xml(path_to_processed_test_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                             test_data_file.split(".")[:-1][0] + ".xml",
-                                             path_to_processed_test_files + os.path.sep + KRNNT_FOLDER + os.path.sep +
-                                             test_data_file.split(".")[:-1][0] + ".xml"
-                                             )
+    words = []
+    labels = []
 
-            get_tagging_from_morphoDita(text,
-                                        path_to_processed_test_files + os.path.sep + MORPHODITA_FOLDER + os.path.sep +
-                                        test_data_file.split(".")[:-1][0] + ".xml")
-            get_tagging_from_wcrft2(text, path_to_processed_test_files + os.path.sep + WCRFT2_FOLDER + os.path.sep +
-                                    test_data_file.split(".")[:-1][0] + ".xml")
-
-
-def read_training_data(path_to_data: str, tagger_folder: str):
-    training_data_files = os.listdir(path_to_data + os.path.sep + 'processed_train' + os.path.sep + tagger_folder)
-
-    train_words = []
-    train_labels = []
-
-    for training_file in training_data_files:
-        print(f"{tagger_folder}, {training_file}")
+    for file in data_files:
+        print(f"{tagger_folder}, {file}")
         xml_text = open(
-            path_to_data + os.path.sep + 'processed_train' + os.path.sep + tagger_folder + os.path.sep + training_file,
+            path_to_data + os.path.sep + f'processed_{dataset_part}' + os.path.sep + tagger_folder + os.path.sep + file,
             "rb").read()
         root = etree.fromstring(xml_text)
 
@@ -87,50 +62,19 @@ def read_training_data(path_to_data: str, tagger_folder: str):
                 for token_index in range(number_of_tokens):
                     if sentence[token_index].tag != 'ns':
                         words_with_tags.append((sentence[token_index][1][0].text, sentence[token_index][1][1].text))
-        train_words.append(words_with_tags)
-        train_labels.append(training_file.split("_")[0])
+        words.append(words_with_tags)
+        labels.append(file.split("_")[0])
 
-    return train_words, train_labels
-
-
-def read_testing_data(path_to_data: str, tagger_folder: str):
-    test_data_files = os.listdir(path_to_data + os.path.sep + 'processed_test' + os.path.sep + tagger_folder)
-
-    test_words = []
-    test_labels = []
-
-    for test_file in test_data_files:
-        print(f"{tagger_folder}, {test_file}")
-        xml_text = open(
-            path_to_data + os.path.sep + 'processed_test' + os.path.sep + tagger_folder + os.path.sep + test_file,
-            "rb").read()
-        root = etree.fromstring(xml_text)
-
-        words_with_tags = []
-        number_of_chunks = len(root)
-        for chunk_index in range(number_of_chunks):
-            chunk = root[chunk_index]
-            number_of_sentences = len(chunk)
-            for sentence_index in range(number_of_sentences):
-                sentence = chunk[sentence_index]
-                number_of_tokens = len(sentence)
-                for token_index in range(number_of_tokens):
-                    if sentence[token_index].tag != 'ns':
-                        words_with_tags.append((sentence[token_index][1][0].text, sentence[token_index][1][1].text))
-        test_words.append(words_with_tags)
-        test_labels.append(test_file.split("_")[0])
-
-    return test_words, test_labels
-
+    return words, labels
 
 if __name__ == '__main__':
-    train_words_morpho, train_labels_morpho = read_training_data(PATH_TO_RAW_FILES, "morphoDita")
-    train_words_wcrft2, train_labels_wcrft2 = read_training_data(PATH_TO_RAW_FILES, "wcrft2")
-    train_words_krnnt, train_labels_krnnt = read_training_data(PATH_TO_RAW_FILES, "krnnt")
+    train_words_morpho, train_labels_morpho = read_data(PATH_TO_RAW_FILES, "morphoDita", "train")
+    train_words_wcrft2, train_labels_wcrft2 = read_data(PATH_TO_RAW_FILES, "wcrft2", "train")
+    train_words_krnnt, train_labels_krnnt = read_data(PATH_TO_RAW_FILES, "krnnt", "train")
 
-    test_words_morpho, test_labels_morpho = read_testing_data(PATH_TO_RAW_FILES, "morphoDita")
-    test_words_wcrft2, test_labels_wcrft2 = read_testing_data(PATH_TO_RAW_FILES, "wcrft2")
-    test_words_krnnt, test_labels_krnnt = read_testing_data(PATH_TO_RAW_FILES, "krnnt")
+    test_words_morpho, test_labels_morpho = read_data(PATH_TO_RAW_FILES, "morphoDita", "test")
+    test_words_wcrft2, test_labels_wcrft2 = read_data(PATH_TO_RAW_FILES, "wcrft2", "test")
+    test_words_krnnt, test_labels_krnnt = read_data(PATH_TO_RAW_FILES, "krnnt", "test")
 
     with open('../data/wikipedia_data/morpho.pkl', 'wb') as f:
         pkl.dump({"train_words": train_words_morpho, "train_labels": train_labels_morpho,
