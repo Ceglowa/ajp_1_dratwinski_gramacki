a907b739649c918523af6aa2bf0797e9d9e9b908
Piotr Gramacki <p.gramacki@gmail.com>
2020-11-01 23:10:54 +0100

    add vectorizer and clf

--- bayesian_model/vectorizer.py
+++ bayesian_model/vectorizer.py
@@ -1,8 +1,13 @@
 from typing import Tuple, List, Union, Callable
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+import numpy as np
 import os
 import pickle as pkl
+
+from sklearn.preprocessing import LabelEncoder
 
 PATH_TO_PICKLES = "../data/wikipedia_data"
 
@@ -33,13 +38,40 @@ def extract_words_based_on_rules(words_list: List[List[Tuple[str, str]]], rules_
     return processed_words_list
 
 
+def get_vectorized_data(tagger_name: str, rules: List):
+    train_words, train_labels, test_words, test_labels = read_pickled_data(PATH_TO_PICKLES + os.path.sep + f"{tagger_name}.pkl")
+
+    train_words_filtered_POS = extract_words_based_on_rules(train_words, rules)
+    test_words_filtered_POS = extract_words_based_on_rules(test_words, rules)
+
+    vectorizer = CountVectorizer()
+    encoder = LabelEncoder()
+
+    train_words_filtered_POS = [' '.join(x) for x in train_words_filtered_POS]
+    test_words_filtered_POS = [' '.join(x) for x in test_words_filtered_POS]
+
+    X_train = vectorizer.fit_transform(train_words_filtered_POS).toarray()
+    X_test = vectorizer.transform(test_words_filtered_POS).toarray()
+
+    y_train = encoder.fit_transform(train_labels)
+    y_test = encoder.transform(test_labels)
+
+    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
+
+
 if __name__ == '__main__':
-    train_words, train_labels, test_words, test_labels = read_pickled_data(PATH_TO_PICKLES + os.path.sep + "krnnt.pkl")
+    for tagger in ['krnnt', 'morpho', 'wcrft2']:
+        X_train, y_train, X_test, y_test = get_vectorized_data(tagger, [lambda tag: tag.startswith("subst")])
 
-    nouns_rules_list = [lambda tag: tag.startswith("subst")]
+        print("got data")
 
-    train_words_only_nouns = extract_words_based_on_rules(train_words, nouns_rules_list)
-    test_words_only_nouns = extract_words_based_on_rules(test_words, nouns_rules_list)
-    train_vocalbuary = set([train_words_only_nouns[train_words_idx][train_word_idx] for train_words_idx in
-                            range(len(train_words_only_nouns)) for train_word_idx in
-                            range(len(train_words_only_nouns[train_words_idx]))])
+        clf = MultinomialNB()
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+
+        print(tagger)
+        print(classification_report(y_true=y_test, y_pred=y_pred))
+
+
+
